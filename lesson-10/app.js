e7b63b68f61ef91e572d88b0f9c73841a857485e
teacherakhror <akhrorteacher@gmail.com>
2022-02-22 08:59:28 +0500

    lesson 10 files

---
+++ lesson-10/app.js
@@ -0,0 +1,39 @@
+/* ------ ko'p ishlatilinadigan string metodlari ------ */
+
+let email = 'akhrorweb@gmail.com'
+
+// lastIndeOf()
+/*let result = email.lastIndexOf('o')
+console.log(result) */
+// indexOf
+
+// slice()
+/*;('0a1k2h3r4o5r6w7e8b9@10gmail.com')
+let result = email.slice(10)
+console.log(result) */
+
+// substr()
+// let result = email.substr(1, 9).length
+// console.log(result)
+
+// replace()
+// let result = email.replace('a', 'd')
+// console.log(result)
+
+// charAt() - birinchi harakterni olib beradi
+// let result = email.charAt()
+// console.log(result)
+
+// toUpperCase() - toLowerCase()
+
+// let result = email.toLowerCase()
+// console.log(result)
+
+// trim()
+
+// let result = email.trim()
+// console.log(result.length, email.length)
+
+// split()
+// let result = email.split('a')
+// console.log(result)
