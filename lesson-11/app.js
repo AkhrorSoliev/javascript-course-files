564d5d189c2ff4c9099f48b697c6081b92b68ae4
teacherakhror <akhrorteacher@gmail.com>
2022-02-23 10:14:33 +0500

    lesson 11 files

---
+++ lesson-11/app.js
@@ -0,0 +1,36 @@
+let radius = 10
+const pi = 3.14
+
+// console.log(radius, pi)
+
+// math operators +,-,*,/,**,%
+
+// console.log(10 / 2)
+
+// let result = 199 % 100
+// let result = pi * radius ** 2
+
+// order of operation
+
+// let result = 5 * (10 - 3) ** 2
+
+// let likes = 10
+
+// likes = likes + 1
+// likes++
+// likes--
+// likes = likes + 10
+// likes += 12
+// likes -= 8
+// likes /= 5
+// likes *= 3
+
+// console.log(likes)
+
+// NaN - not a number
+
+console.log(5 / 'Salom')
+console.log(5 * 'Salom')
+
+let result = 'the blog has ' + likes + ' likes' + ' '
+// console.log(result)
