f56eeb9801ddb906a1b77d050b213e1e7b917f8b
teacherakhror <akhrorteacher@gmail.com>
2022-02-23 21:05:00 +0500

    lesson 11 files update

--- lesson-11/app.js
+++ lesson-11/app.js
@@ -1,36 +1,19 @@
-let radius = 10
-const pi = 3.14
+// template string
+const title = 'Rastamojka qachon ochiladi ?! yohud Monopoliya Respublikasi !'
+const author = "Lag'mon makazi"
+const likes = 35000000
 
-// console.log(radius, pi)
+// concatentation way
+// let result = 'Bu post nomi ' + title + ' ' + author + ' tomonidan yozilgan. ' + 'va bu postda ' + likes + ' ta like bor'
 
-// math operators +,-,*,/,**,%
-
-// console.log(10 / 2)
-
-// let result = 199 % 100
-// let result = pi * radius ** 2
-
-// order of operation
-
-// let result = 5 * (10 - 3) ** 2
-
-// let likes = 10
-
-// likes = likes + 1
-// likes++
-// likes--
-// likes = likes + 10
-// likes += 12
-// likes -= 8
-// likes /= 5
-// likes *= 3
-
-// console.log(likes)
-
-// NaN - not a number
-
-console.log(5 / 'Salom')
-console.log(5 * 'Salom')
-
-let result = 'the blog has ' + likes + ' likes' + ' '
 // console.log(result)
+
+// template string way
+// let result = `Bu post nomi ${title}, ${author} tomonidan yozilgan. Va bu postda ${likes} ta like bor`
+
+let result = `
+    <h2>${title}</h2>
+    <p>${author} tomonidan yozilgan</p>
+    <span>${title}</span>
+`
+console.log(result)
