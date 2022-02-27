1d04ba4a61ed39f33d03f374205f847e005729c5
teacherakhror <akhrorteacher@gmail.com>
2022-02-27 15:45:45 +0500

    lesson 17 files

---
+++ lesson-17/app.js
@@ -0,0 +1,13 @@
+// type conversion - turlarni o'zgartirish
+let natija = '100'
+
+// natija = Number(natija)
+
+// console.log(natija + 1)
+
+// let result = String(15)
+// console.log(typeof result)
+
+let result = Number('123')
+
+console.log(result)
