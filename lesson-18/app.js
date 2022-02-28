2d8742e103fb56a03b61375c3b3057518afb8924
teacherakhror <akhrorteacher@gmail.com>
2022-02-28 10:16:02 +0500

    mashqlar 1 files

---
+++ lesson-18/app.js
@@ -0,0 +1,11 @@
+// for loop
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i)
+// }
+
+const names = ['ahror', 'sardor', 'doniyor']
+
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i])
+}
