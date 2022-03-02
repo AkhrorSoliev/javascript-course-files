70662753a1278fbc7f0a92ffd0ff90dd8c273ecc
teacherakhror <akhrorteacher@gmail.com>
2022-03-02 22:21:34 +0500

    lesson 26 files

--- lesson-24/app.js
+++ lesson-24/app.js
@@ -1,32 +1,16 @@
-// switch case
+// Block Scope
+const age = 24
 
-const baho = 'R'
+if (true) {
+  const age = 25
+  const ism = 'ahror'
+  console.log('If elsening ichidagi age:', age, ism)
 
-switch (baho) {
-  case 'A':
-    console.log('Sizning bahoyingiz A')
-    break
-  case 'B':
-    console.log('Sizning bahoyingiz B')
-    break
-  case 'C':
-    console.log('Sizning bahoyingiz C')
-    break
-  case 'D':
-    console.log('Sizning bahoyingiz D')
-    break
-  default:
-    console.log('Sizning bahoyingiz E')
+  if (true) {
+    const age = 30
+    console.log('Ikkinchi if ichi:', age)
+    var test = 'Hello'
+  }
 }
 
-// if (baho === "A") {
-// //
-// } else if (baho === 'B') {
-// //
-// } else if (baho === 'C') {
-// //
-// } else if (baho === 'D') {
-// //
-// } else {
-// //
-// }
+console.log('Tashqi tomonda chaqirilgan age:', age, test)
