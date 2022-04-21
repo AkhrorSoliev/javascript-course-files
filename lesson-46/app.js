9ee266385c092df70f3761d40e25b926b7ca460a
teacherakhror <akhrorteacher@gmail.com>
2022-04-22 00:36:36 +0500

    lesson 46 files

---
+++ lesson-46/app.js
@@ -0,0 +1,10 @@
+/*--------------- lesson-46 ------------*/
+const heading = document.querySelector('h1')
+
+// heading.setAttribute('style', 'margin: 50px')
+console.log(heading.style)
+heading.style.margin = '50px'
+heading.style.color = 'crimson'
+heading.style.fontSize = '50px'
+heading.style.margin = ''
+heading.style.backgroundColor = '#ccc'
