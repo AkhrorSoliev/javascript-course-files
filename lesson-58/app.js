49e6f072da67cd2881b2237c91cd1616b453b357
teacherakhror <akhrorteacher@gmail.com>
2022-06-03 23:45:12 +0500

    lesson 58 files

--- lesson-58/app.js
+++ lesson-58/app.js
@@ -1,8 +1,18 @@
-// keydown, keyup, keypress
+const insert = document.getElementById('insert')
 
-document.addEventListener('keyup', (e) => {
-  if (e.key == 'm' && e.ctrlKey) console.log('GOOOL !!!')
-  //   console.log('Code', e.code)
-  //   console.log('Key', e.key)
-  //   console.log('keyCode', e.keyCode)
+window.addEventListener('keydown', (e) => {
+  insert.innerHTML = `
+    <div class="key">
+        ${e.key == ' ' ? 'Space' : e.key}
+      <small>event.key</small>    
+    </div>
+    <div class="key">
+        ${e.keyCode}
+      <small>event.keyCode</small>    
+    </div>
+    <div class="key">
+        ${e.code}
+      <small>event.code</small>    
+    </div>
+  `
 })
